665b6eb2b3765946269b4ff040e443b56b9ea7fa
GazApp <juangaz@hotmail.com>
2020-04-10 19:36:36 -0500

    Basic 1

---
+++ Basic/src/main/java/gaz/app/net/basic/HelloWorldandGit.java
@@ -0,0 +1,12 @@
+
+
+package gaz.app.net.basic;
+
+
+public class HelloWorldandGit {
+   
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World and Git");
+    }
+}
